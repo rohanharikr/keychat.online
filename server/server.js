ae85a26f66ffb3a2c661717be61b8c06edbf7a9c
thoran <rohanharikumar80@gmail.com>
2020-09-25 01:32:45 +0530

    limit and lock session, ui improvement

--- server/server.js
+++ server/server.js
@@ -4,25 +4,47 @@ const io = require('socket.io')(http);
 
 io.on('connection', socket => {
     let secretRoom;
-    socket.on('secretRoom', secretKey => {
+    socket.on('joinRoom', joinKey => {
+        if (io.sockets.adapter.rooms[joinKey] && io.sockets.adapter.rooms[joinKey].length <= 1) {
+            secretRoom = joinKey;
+            socket.join(secretRoom);
+
+            socket.broadcast
+                .to(secretRoom)
+                .emit(
+                    //this fires a notification on the frontend , 0 = disconnect, 1 = joined
+                    'botMessage', 1);
+        } else {
+            socket.emit('sessionLocked');
+        }
+    });
+
+    socket.on('newRoom', secretKey => {
         secretRoom = secretKey;
         socket.join(secretRoom);
+        //get total numbers os clients in a room
+        // const room = io.sockets.adapter.rooms[secretRoom].length;
         socket.broadcast
             .to(secretRoom)
             .emit(
-                'botMessage', 'someone has joined the chat');
+                //this fires a notification on the frontend , 0 = disconnect, 1 = joined
+                'botMessage', 1);
     });
+
+    //message to room
     socket.on('message', message => {
-        // io.to(secretRoom).emit('sendMessage', message);
         socket.broadcast.to(secretRoom).emit('sendMessage', message);
     });
+
+    //fired when a user disconnects
     socket.on('disconnect', () => {
         socket.broadcast
             .to(secretRoom)
             .emit(
-                'botMessage', 'someone has left the chat');
-    })
-})
+                //this fires a notification on the frontend , 0 = disconnect, 1 = joined
+                'botMessage', 0);
+    });
+});
 
 http.listen(3001, () => {
     console.log('listening on 3001');
